1db1f668b5d20efb844347db924eff54cddcd0c9
Harish Goutham V <harishgoutham.v@gmail.com>
2021-03-14 16:24:19 +0530

    Solution to issue #34

--- 003/031_coin_sums.py
+++ 003/031_coin_sums.py
@@ -11,3 +11,12 @@ It is possible to make £2 in the following way:
 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
 How many different ways can £2 be made using any number of coins?
 '''
+target = 201
+ways = [0] * target
+ways[0] = 1
+
+for x in [1,2,5,10,20,50,100,200]:
+  for y in range(x,target):
+    ways[y] += ways[y - x]
+
+print(ways[target-1])
