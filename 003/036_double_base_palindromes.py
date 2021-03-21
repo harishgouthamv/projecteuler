539872d1f6e1cf4beda7391d6e5397df1123b68e
Harish Goutham V <harishgoutham.v@gmail.com>
2021-03-21 22:11:53 +0530

    Solution to issue #39

--- 003/036_double_base_palindromes.py
+++ 003/036_double_base_palindromes.py
@@ -11,3 +11,18 @@ which are palindromic in base 10 and base 2.
 (Please note that the palindromic number, in either base,
 may not include leading zeros.)
 '''
+s = 0
+for x in range(1,1000001):
+  y = [y for y in str(x)]
+  y.reverse()
+  if(int("".join(y)) == x):
+    #print("acc: " + str(x))
+    ba = bin(x).replace("0b", "")
+    na = int(ba)
+    m = [m for m in str(ba)]
+    m.reverse()
+    rna = int("".join(m))
+    if(na == rna):
+      s += x
+
+print(s)
