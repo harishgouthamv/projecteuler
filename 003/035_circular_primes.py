e1808e0006c6c7b3acf5dcd7485a345834830c72
Harish Goutham V <harishgoutham.v@gmail.com>
2021-03-21 22:11:37 +0530

    Solution to issue #38

--- 003/035_circular_primes.py
+++ 003/035_circular_primes.py
@@ -10,3 +10,39 @@ There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73
 
 How many circular primes are there below one million?
 '''
+
+def rotate(num):
+  l = []
+  s = c = len(str(num))
+  while s != 1:
+    s -= 1
+    w = (10 ** (c - 1))
+    a = int(num / w)
+    r = ((num % w) * 10) + a
+    num = r
+    l.append(r)
+  return l
+
+def isprime(num):
+  if(num == 2):
+    return True
+  elif(num % 2 == 0):
+    return False
+  else:
+    lim = round(num / 2) + 1
+    for x in range(3, lim):
+      if(num % x == 0):
+        return False
+  return True
+
+c = 0
+
+for num in range(2, 1000001):
+  if (isprime(num)):
+    for x in rotate(num):
+      if not isprime(x):
+        break
+    else:
+      c += 1
+
+print(c)
