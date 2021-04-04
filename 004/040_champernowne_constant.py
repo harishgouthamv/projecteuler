6d2e57e719c45b1f802f295f1ed8d65ad29fabc3
Harish Goutham V <harishgoutham.v@gmail.com>
2021-04-04 19:47:15 +0530

    Solution to issue #43

--- 004/040_champernowne_constant.py
+++ 004/040_champernowne_constant.py
@@ -5,3 +5,12 @@ Issue - #43
 Description -
 https://projecteuler.net/problem=40
 '''
+n = 1
+s = str(0)
+m = 1
+while len(s) <= 1000001:
+  s += str(n)
+  n+=1
+darr = [int(d) for d in s]
+p = darr[1] * darr[10] * darr[100] * darr[1000] * darr[10000] * darr[100000] * darr[1000000]
+print(p)
