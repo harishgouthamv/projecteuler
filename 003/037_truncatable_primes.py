d6186f85033a391dd21b074f78ba6a31b84ba7e8
Harish Goutham V <harishgoutham.v@gmail.com>
2021-04-04 17:46:17 +0530

    Solution to issue #40

--- 003/037_truncatable_primes.py
+++ 003/037_truncatable_primes.py
@@ -9,3 +9,38 @@ Find the sum of the only eleven primes that are both truncatable from left to ri
 
 NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 '''
+
+def isprime(num):
+  if(num == 1):
+    return False
+  elif(num == 2):
+    return True
+  elif(num % 2 == 0):
+    return False
+  else:
+    lim = round(num / 2) + 1
+    for x in range(3, lim):
+      if(num % x == 0):
+        return False
+  return True
+
+c = 0
+num = 11
+csum = 0
+
+while c != 11:
+  if isprime(num):
+    l = len(str(num))
+    s = set()
+    left = [num % (10**x) for x in range(1, l+1)]
+    right = [int(num / (10**x)) for x in range(l)]
+    for n in set(left + right):
+      if not isprime(n):
+        break
+    else:
+      c += 1
+      csum += num
+      print(num)
+  num += 1
+
+print(csum)
