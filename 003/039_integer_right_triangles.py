73cca9c4c7d9981b1f87870a1c86f387c8705198
Harish Goutham V <harishgoutham.v@gmail.com>
2021-04-04 20:18:18 +0530

    Solution to issue #42

--- 003/039_integer_right_triangles.py
+++ 003/039_integer_right_triangles.py
@@ -10,3 +10,20 @@ integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
 
 For which value of p ≤ 1000, is the number of solutions maximised?
 '''
+from itertools import combinations_with_replacement, permutations
+
+py = lambda x : ((x[0] ** 2) + (x[1] ** 2)) == (x[2] ** 2)
+
+l = 0
+
+for x in range(3, 1001):
+  s = set()
+  #s |= {p for p in permutations(range(1, x+1), 3) if sum(p) == x }
+  s |= {p for p in combinations_with_replacement(range(1, x+1), 3) if sum(p) == x }
+  plist = [list(p) for p in s if py(p)]
+  nl = len(plist)
+  if nl > l:
+    l = nl
+    print(l)
+    print(plist)
+    print(x)
