08e2cf324d41ae0fe1fbc73d851ba5c1f82e3e71
Harish Goutham V <harishgoutham.v@gmail.com>
2021-04-06 18:54:51 +0530

    Solution to issue #44

--- 004/041_pandigital_prime.py
+++ 004/041_pandigital_prime.py
@@ -9,3 +9,26 @@ For example, 2143 is a 4-digit pandigital and is also prime.
 
 What is the largest n-digit pandigital prime that exists?
 '''
+def isprime(num):
+  if(num == 1):
+    return False
+  elif(num == 2):
+    return True
+  elif(num % 2 == 0):
+    return False
+  else:
+    lim = round(num / 2) + 1
+    for x in range(3, lim):
+      if(num % x == 0):
+        return False
+  return True
+
+getpan = lambda n: {x for x in range(1,len(str(n))+1)}
+sList= [0] + [sum(getpan(10 ** n)) for n in range(10)]
+ispansum = lambda n: sList[len(str(n))] == sum({int(y) for y in str(n)})
+ispan = lambda n: getpan(n).symmetric_difference({int(y) for y in str(n)}) == set()
+
+
+for n in range(111, 987654322, 2):
+  if isprime(n) and ispansum(n) and ispan(n):
+    print(n)
