21819c55326bce47ac8cd95c74094aac4194e98f
Harish Goutham V <harishgoutham.v@gmail.com>
2021-04-06 21:18:31 +0530

    Solution to issue #47

--- 004/045_xagonal.py
+++ 004/045_xagonal.py
@@ -3,3 +3,23 @@
 Problem  - Triangular, pentagonal, and hexagonal
 Issue - #48
 '''
+from math import sqrt
+
+def istri(n):
+  x = (sqrt((8 * n) + 1) - 1) / 2
+  return x == int(x)
+
+def isPen(n):
+  p = (sqrt(1.0 + (24.0 * n)) + 1.0) / 6.0
+  return p == int(p)
+
+def isHex(n):
+  x = (sqrt((8 * n) + 1) + 1) / 4
+  return x == int(x)
+
+c = 40756
+while True:
+  if istri(c) and isPen(c) and isHex(c):
+    print(c)
+    break
+  c += 1
