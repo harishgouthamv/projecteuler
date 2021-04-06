61d2ff25566be212027dde8b4cf05a1f003db10c
Harish Goutham V <harishgoutham.v@gmail.com>
2021-04-06 20:31:28 +0530

    Solution to issue #47

--- 004/044_pentagon_numbers.py
+++ 004/044_pentagon_numbers.py
@@ -3,3 +3,26 @@
 Problem  - Pentagon numbers
 Issue - #47
 '''
+from math import sqrt
+
+def isPentagonal(n):
+  p = (sqrt(1.0 + (24.0 * n)) + 1.0) / 6.0
+  return p == int(p)
+
+p = lambda n: n * (3 * n - 1) / 2
+result = 0
+i = 0
+
+while True:
+  i += 1
+  n = p(i)
+  for j in reversed(range(1, i)):
+    m = p(j)
+    if isPentagonal(n - m) and isPentagonal(n + m):
+      result = n - m
+      break
+  else:
+    continue
+  break
+
+print(result)
